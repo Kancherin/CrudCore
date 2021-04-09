495573c3d1ae88073729fb99f45f508c9a46ea44
David <david_horeb@hotmail.com>
2021-04-09 13:19:04 -0300

    Add EmpleadosDAL

--- CrudCore.Web/Models/EmpeladosDAL.cs
+++ CrudCore.Web/Models/EmpeladosDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,87 @@ namespace CrudCore.Web.Models
     {
         string _connectionString = "Data Source=david-pc;Initial Catalog=EmpleadosDB;Integrated Security=True";
 
+        //Devolver Todos
         public IEnumerable<EmpleadosInfo> GetEmpleados()
         {
             List<EmpleadosInfo> _empleados = new List<EmpleadosInfo>();
-            
+            using(SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetEmpleados", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    EmpleadosInfo emp = new EmpleadosInfo();
+                    emp.Id = Convert.ToInt32(dr["Id"].ToString());
+                    emp.Name = dr["Name"].ToString();
+                    emp.Gender = dr["Gender"].ToString();
+                    emp.Company = dr["Company"].ToString();
+                    emp.Department = dr["Department"].ToString();
+
+                    _empleados.Add(emp);
+                }
+                con.Close();
+            }
+            return _empleados;
         }
+
+        //Agregar
+        public void AddEmpleados(EmpleadosInfo emp)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_AddEmpleado", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);
+                cmd.Parameters.AddWithValue("@Company", emp.Company);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                con.Close();
+            }
+        }
+
+        //Actualizar
+        public void UpdateEmpleado(EmpleadosInfo emp)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateEmpleado", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);
+                cmd.Parameters.AddWithValue("@Company", emp.Company);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                con.Close();
+            }
+        }
+
+        //Borrar
+        public void DeleteEmpleado(int? Id)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteEmpleado", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", Id);
+                
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                con.Close();
+            }
+        }
+
     }
 }
