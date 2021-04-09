dafdb4d160db15d315eb65ab388bd23fe8a98f31
David <david_horeb@hotmail.com>
2021-04-09 11:38:00 -0300

    Add Vista Index

---
+++ CrudCore.Web/Models/EmpeladosDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCore.Web.Models
+{
+    public class EmpeladosDAL
+    {
+        string _connectionString = "Data Source=david-pc;Initial Catalog=EmpleadosDB;Integrated Security=True";
+
+        public IEnumerable<EmpleadosInfo> GetEmpleados()
+        {
+            List<EmpleadosInfo> _empleados = new List<EmpleadosInfo>();
+            
+        }
+    }
+}
