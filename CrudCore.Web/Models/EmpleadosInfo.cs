dad59e06df5753470f85b4e1f5021601ce65a62d
David <david_horeb@hotmail.com>
2021-04-09 08:17:12 -0300

    Add Web Proyect + EmpleadoController

---
+++ CrudCore.Web/Models/EmpleadosInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCore.Web.Models
+{
+    public class EmpleadosInfo
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Gender { get; set; }
+        [Required]
+        public String Company { get; set; }
+        [Required]
+        public String Department { get; set; }
+    }
+}
